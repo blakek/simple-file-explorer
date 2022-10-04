7d789796ad0be6abcb03a91c632e2b44f80d4e5c
Blake Knight <oss.ideas@gmail.com>
2022-10-04 12:24:47 -0500

    :art: simplify basic template

--- src/templates/Basic.tsx
+++ src/templates/Basic.tsx
@@ -9,31 +9,13 @@ export interface BasicTemplateProps {
   title?: string;
 }
 
-function Basic({
-  children,
-  keywords = [],
-  metaDescription,
-  socialMediaImage,
-  title,
-}: BasicTemplateProps): JSX.Element {
-  const metaKeywords = Array.isArray(keywords) ? keywords.join(",") : "";
-
+function Basic({ children, title }: BasicTemplateProps): JSX.Element {
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>{title}</title>
-
-        <meta name="description" content={metaDescription} />
-        <meta name="keywords" content={metaKeywords} />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:description" content={metaDescription} />
-        <meta name="twitter:title" content={title} />
-        <meta property="og:description" content={metaDescription} />
-        <meta property="og:title" content={title} />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content={socialMediaImage} />
       </Head>
 
       <main>{children}</main>

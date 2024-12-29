29c12ba0ffc74f67dc5b72f5492098d8c09609b3
Blake Knight <oss.ideas@gmail.com>
2024-12-28 23:19:08 -0600

    :zap: configure Next.js to skip linting and type checking during builds for faster build times

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ module.exports = async (phase, { defaultConfig }) => {
     compiler: {
       styledComponents: true,
     },
+
+    // Speed up build by skipping linting and type checking
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
   };
 
   return config;
